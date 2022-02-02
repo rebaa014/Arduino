e8f925386dad3ac2e2b903b82f6c9b4ca166dbd4
Reha Balci <rebaa014@osloskolen.no>
2022-02-02 09:23:33 +0100

    Create sketch_sep08a.ino

---
+++ The Codes/sketch_sep08a/sketch_sep08a.ino
@@ -0,0 +1,13 @@
+int led = 8;
+void setup()
+{
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(led, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
